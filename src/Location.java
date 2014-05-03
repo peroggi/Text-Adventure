9c3b40b5d6496ce6b068b1215dfa6eb0a47bccf6
perogi <mattpiroddi@gmail.com>
2014-05-02 22:20:08 -0700

    Signed-off-by: perogi <mattpiroddi@gmail.com>

--- src/Location.java
+++ src/Location.java
@@ -1,36 +1,66 @@
-
+import java.util.ArrayList;
+import java.util.List;
 public class Location extends Thing{
-	// what data type to use for links tbd, I'm thinking an array of Location objects?
-	Location[] links = null;
+
+	private List<Location> links = new ArrayList<Location>();
+	private List<Thing> contents = new ArrayList<Thing>();
+	private boolean discovered = false;
 	
-	Item[] contain = null;
+	Location(String name, String desc, Thing[] contents){
+		this.name = name;
+		this.desc = desc;
+		for(Thing o : contents){
+			this.contents.add(o);
+		}
+	}
 	
-	boolean discovered = false;
-	
-	// i'm guessing your remove method was to remove an item from a location? 
-	boolean remove(Object obj) {
-		for (Item i : contain) {
-			if (i.equals(obj)) {
-				i = null;
+	void remove(Thing o) {
+		this.contents.remove(o);
+	}
+	void add(Thing o){
+		this.contents.add(o);
+		}	
+		
+	boolean isLinked(String txt){ //will be called when player attempts to move to a location
+		for(Location l : this.links){
+			if(l.getName() == txt){
 				return true;
 			}
 		}
-		return false;
+	return false;
 	}
 	
-	boolean look() {
+	boolean inContents(String txt){
+		for(Thing t: contents){
+			if(t.getName() == txt){
+				return true;
+			}
+		}
+	return false;
+	}
+		
+	void discover(){
+		this.discovered = true;
+		}
+		
+	void linkLocation(Location loc){
+		this.links.add(loc);
+		}
+	
+	String look() {
 		String txt = this.desc + " You can also see the following things: ";
 		
-		for (Item i : contain) {
-			if (i != null) {
-				txt = txt + " " + i;
-			}	
+		for (Object i : contents) {
+			txt = txt + " " + i;
 		}
-	return true;
+		return txt;
 	}
 	
-	boolean joinLoc(/*locs*/) {
-		// TODO 
-
+	void joinLoc(Location[] locs) {
+		for(Location l : locs){
+			this.linkLocation(l);
+			l.linkLocation(this);
+		}
 	}
+		
 }
--- src/Thing.java
+++ src/Thing.java
@@ -4,26 +4,27 @@ public class Thing {
 	String desc;
 	boolean getable = false;
 	
-// why do these methods in omnithing return a boolean?	
-/*oh well I guess to say whether the action succeeded or failed. If you try to get a thing
-and you can't get it, it returns false. If you can and you do pick up the thing, it returns true.*/
-	boolean get() {
-		System.out.println("You can't pick that up");
-		return false;
+	String getName(){
+		return this.name;
 	}
 	
-	boolean look() {
-		System.out.println(this.desc);
-		return true;
+	boolean get() {
+		return getable;
+		/*so this is what we were kinda talking about. This way it just tells the input/outplut class (whatever we make it)
+		 * that getting the object failed, and the input/output class would deal with the output related to that
+		 * I think this might be better so that all the output comes from oen sort of area in our code, what you think?
+		 */
+	}
+	
+	String look() {
+		return this.desc;
 	}
 
 	boolean talk () {
-		System.out.println("If you try to talk to that, people might think you are crazy.");
-		return false;
+		return false; //should we make a boolean talkable? idk if that's more uh conventiony
 	}
 	
 	boolean use() {
-		System.out.println("I'm not going to let you use that for whatever sick, twisted purpose you have imagined.");
 		return false;
 	}
 	
@@ -32,19 +33,8 @@ and you can't get it, it returns false. If you can and you do pick up the thing,
 		return false;
 	}
 	
-	boolean move(Location startLoc, Location endLoc) {
-	/*
-		if (typeof startloc==="undefined") { 
-			startloc = currentloc; 
-		}
-		startloc.contain.splice(startloc.contain.indexOf(this), 1);
-		endloc.contain.push(this);
-		return true;}
-	/* say what? */
-		/*this is for moving things (not the player) to other places. 
-		So when you rescue someone it calls person.move() to make them move elsewhere.
-		I don't know how this works in java but in js you could have optional arguments, startLoc was
-		an optional argument. So the if statement checks to see if startLoc was passed, if it wasn't it 
-		assigns it a default value (where the player currently is). The rest of the method just removes
-		the thing from wherever it was, and adds it to it's new location.*/
+	void move(Location startLoc, Location endLoc) {
+		startLoc.remove(this);
+		endLoc.add(this);
+	}
 }
